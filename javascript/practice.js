0692948de74b87e0a1e4bcad79e5763a0449c551
Kalaiarasan-P <139548206+Kalaiarasan-P@users.noreply.github.com>
2024-03-27 12:29:07 +0530

    Add files via upload

---
+++ javascript/practice.js
@@ -0,0 +1,15 @@
+let arr=['kalai','akil','ezhil','kamal']
+let arr1=['subash','arul']
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr.push('gopi'))
+console.log(arr)
+console.log(arr.concat(arr1))
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+console.log(arr.indexOf('akil'))
